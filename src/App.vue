cac19fd66f49d28f0db8d43f71ffc69eab91a8a4
Ayush <ayushmondal24022004@gmail.com>
2024-06-17 00:31:51 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 v-if="!gameOn">
+    Test Your Reaction Time: Be On Your guard. Click the first green rectangle you see
+  </h1>
+  <button @click='toggleGame' v-if="!gameOn"> Start Game</button>
+  <game v-if="gameOn && !endScreen" @stop='stopTimer'/>
+  <div v-if="endScreen">
+    <h1>{{finalText}}</h1>
+    <p>Your reaction time: {{interval}} milliseconds</p>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Game from './components/Game'
 
 export default {
+  
   name: 'App',
-  components: {
-    HelloWorld
+  components: {Game},
+  data()
+  {
+    return{
+      gameOn: false,
+      startTime: Date.now(),
+      stopTime: Date.now(),
+      interval: 0,
+      endScreen: false,
+      finalText:"default"
+    }
+  },
+  methods:
+  {
+    toggleGame()
+    {
+      this.gameOn=!this.gameOn
+      this.startTime=Date.now()
+    },
+    stopTimer()
+    {
+      this.stopTime=Date.now()
+      this.interval=this.stopTime-this.startTime
+      this.endScreen=!this.endScreen
+      if(this.interval<250)
+      {
+        this.finalText="You've gone supersonic"
+      }
+      if(this.interval<500&&this.interval>=250)
+      {
+        this.finalText="You have Cat like reflexes"
+      }
+      if(this.interval<750&&this.interval>=500)
+      {
+        this.finalText="that's fast"
+      }
+      if(this.interval<1000&&this.interval>=750)
+      {
+        this.finalText="Meh..."
+      }
+      if(this.interval>=1000)
+      {
+        this.finalText="Too slow...Snails pace"
+      }
+    }
   }
+  
 }
+
 </script>
 
 <style>
@@ -22,5 +75,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.box1 
+{
+  top: 50%;
+  left: 50%;
+  height: 30%;
+  width:30%;
+  background: rgb(0, 238, 255);
+}
+.box2
+{
+  top: 0%;
+  left: 0%;
+  height: 30%;
+  width:30%;
+  background: rgb(0, 238, 255);
 }
 </style>
---
+++ src/components/Game.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="container">
+        <div class="box" :class="{boxSel:num===1}" @click='event1(1)'>
+        </div>
+        <div class="box" :class="{boxSel:num===2}" @click='event1(2)'>
+        </div>
+        <div class="box" :class="{boxSel:num===3}" @click='event1(3)'>
+        </div>
+    </div>
+    <div class="container">
+        <div class="box" :class="{boxSel:num===4}" @click='event1(4)'>
+        </div>
+        <div class="box" :class="{boxSel:num===5}" @click='event1(5)'>
+        </div>
+        <div class="box" :class="{boxSel:num===6}" @click='event1(6)'>
+        </div>
+    </div>
+    <div class="container">
+        <div class="box" :class="{boxSel:num===7}" @click='event1(7)'>
+        </div>
+        <div class="box" :class="{boxSel:num===8}" @click='event1(8)'>
+        </div>
+        <div class="box" :class="{boxSel:num===9}" @click='event1(9)'>
+        </div>
+    </div>
+</template>
+<script>
+
+
+
+
+export default {
+  
+  data()
+  {
+    return{
+        num: Math.floor(Math.random() * 9) + 1
+    }
+  },
+  methods:
+  {
+    event1(x)
+    {
+       if(x===this.num)
+        {
+            this.$emit('stop')
+        }
+    }
+
+  }
+  
+  
+  
+}
+</script>
+
+<style>
+
+.container
+{
+    display: flex;
+    justify-content: space-around;
+    margin: 20px;
+}
+.box
+{
+    height:200px;
+    width:30%;
+    background-color: rgb(255, 255, 255);
+}
+.boxSel
+{
+    height:200px;
+    width:30%;
+    background-color: rgb(130, 243, 149);
+}
+</style>
